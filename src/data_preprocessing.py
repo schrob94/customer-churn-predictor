354151e84ab3e3091bab4caffd3878200b04133f
Robin Scheuch <robin.scheuch@gmail.com>
2025-11-10 18:55:46 +0100

    Save current work

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 def clean_data(df):
+    """other modification"""
     """Remove rows with missing customer IDs and fill NaNs."""
     df = df.dropna(subset=['customer_id'])
     df = df.fillna(0)
