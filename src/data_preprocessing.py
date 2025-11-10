8074827890bfa1b8ff1f72cd0e0ee8670775870e
Robin Scheuch <robin.scheuch@gmail.com>
2025-11-10 18:47:53 +0100

    Add basic data cleaning function

---
+++ src/data_preprocessing.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def clean_data(df):
+    """Remove rows with missing customer IDs and fill NaNs."""
+    df = df.dropna(subset=['customer_id'])
+    df = df.fillna(0)
+    return df
